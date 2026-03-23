aa502b33d46bdd5c2c8b796088e6417e3abbf1a1
jhowvictor11-spec <jhowvictor11@gmail.com>
2026-03-22 23:48:07 -0300

    Implement product update functionality in EditarProduto

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -5,5 +7,45 @@ public partial class EditarProduto : ContentPage
     public EditarProduto()
     {
         InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+
+            if (produto_anexado == null)
+            {
+                await Shell.Current.DisplayAlert("Erro", "Produto não encontrado", "OK");
+                return;
+            }
+
+            double quantidade, preco;
+
+            if (!double.TryParse(txt_quantidade.Text, out quantidade) ||
+                !double.TryParse(txt_preco.Text, out preco))
+            {
+                await Shell.Current.DisplayAlert("Erro", "Digite valores válidos", "OK");
+                return;
+            }
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = quantidade,
+                Preco = preco
+            };
+
+            await App.Db.Update(p);
+
+            await Shell.Current.DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
